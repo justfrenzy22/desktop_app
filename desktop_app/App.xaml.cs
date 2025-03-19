7b8f69eb6fd03e7b09fe8fd55cb7e716b257042a
justfrenzy22 <87825389+justfrenzy22@users.noreply.github.com>
2025-03-19 19:13:37 +0100

    Add project files.

---
+++ desktop_app/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace desktop_app
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
